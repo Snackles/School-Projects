9c87a74c7dec64cb8b0e48de99ffa68942cb1114
Jack <626689@pdsb.net>
2020-02-11 09:22:53 -0500

    added min/max to float

--- Project1-Poll.py
+++ Project1-Poll.py
@@ -6,11 +6,11 @@
 # Statistics
 enjoysMathStats = {True: 63, False: 12}
 englishFirstLangStats = {True: 94, False: 6}
-agePercentilesStats = {14: 15.8,
-                       24: 11.5,
-                       54: 40.5,
-                       64: 14.1,
-                       130: 18.1}
+generationStats = {14: 15.8,
+                   24: 11.5,
+                   54: 40.5,
+                   64: 14.1,
+                   130: 18.1}
 
 
 def getInputOfType(message, failMessage, desiredType, minVal=None, maxVal=None):
@@ -20,14 +20,13 @@ def getInputOfType(message, failMessage, desiredType, minVal=None, maxVal=None):
         message - the prompt for the user input
         failMessage - the message to give when input is of wrong type
         desiredType - the type of user input expected
-        min - the minimum value, only used if the desired type is an int
-        max - the maximum value, only used if the desired type is an int
+        min - the minimum value, only used if the desired type is a number
+        max - the maximum value, only used if the desired type is a number
     Returns:
         userInput - the user's input that was the correct type
     """
-
     while True:
-        userInput = input(message)
+        userInput = input(message + ' ') # Add a space to the end of the question for readability
         if desiredType == int:
             try:
                 userInput = int(userInput)
@@ -37,17 +36,23 @@ def getInputOfType(message, failMessage, desiredType, minVal=None, maxVal=None):
                         raise ValueError
             except ValueError:
                 print(failMessage)
+                continue
             else:
-                # Input is correct type
+                # Input is correct type and within min and max
                 return int(userInput)
 
         elif desiredType == float:
             try:
-                float(userInput)
+                userInput = float(userInput)
+                # If the value is outside of the min and max
+                if minVal is not None and maxVal is not None:
+                    if not (maxVal >= userInput >= minVal):
+                        raise ValueError
             except ValueError:
                 print(failMessage)
+                continue
             else:
-                # Input is correct type
+                # Input is correct type and within min and max
                 return float(userInput)
 
         elif desiredType == bool:
@@ -58,8 +63,9 @@ def getInputOfType(message, failMessage, desiredType, minVal=None, maxVal=None):
                 return False
             else:
                 print(failMessage)
+                continue
         else:
-            # The desired type is not one of the expected ones (bool, int, float)
+            # The desired type is not one of the expected ones (int, float, bool)
             raise ValueError
 
 
@@ -68,23 +74,23 @@ print('Welcome to the fun poll thingy')
 print('Data sources: 2018 Peel Student Census, 2016 Canadian Census')
 
 enjoysMath = getInputOfType('Do you enjoy math?', 'Sorry, please respond with yes or no', bool)
-if enjoysMath == True:
+if enjoysMath:
     print('63% of PCSS students agree with you!')
 else:
     print('12% of PCSS students agree with you!')
 
 englishFirstLang = getInputOfType('Do you speak english as your first language?',
                                   'Sorry, please respond with yes or no', bool)
-if englishFirstLang == True:
+if englishFirstLang:
     print('So does 94% of the school!')
 else:
     print('6% of the school doesn\'t either!')
 
 # Max age 130, the oldest person ever was 122
-userAge = getInputOfType('How old are you?', 'Sorry, please provide a number from 0 to 130', int, 0, 130)
-for age in agePercentilesStats:
+userAge = getInputOfType('How old are you?', 'Sorry, please provide a whole number from 0 to 130', int, 0, 130)
+for age in generationStats:
     if userAge <= age:
-        print(agePercentilesStats[age], 'percent of Canadians are in the same generation as you!')
+        print(generationStats[age], 'percent of Canadians are in the same generation as you!')
         # Stop the loop so it doesn't print every generation above the correct one
         break
 
